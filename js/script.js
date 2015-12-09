caa6c94a6732b49eb878c9bed168e33caabcb3de
Antowka <antowa.kin@gmail.com>
2015-12-10 03:25:22 +0600

    1
    начало

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var m1 = 112; 
+var m2 = 10; 
+
+function getScrollTop() {
+           var scrOfY = 0;
+           if( typeof( window.pageYOffset ) == "number" ) {
+                   scrOfY = window.pageYOffset;
+           } else if( document.body 
+           && ( document.body.scrollLeft 
+           || document.body.scrollTop ) ) {
+                   scrOfY = document.body.scrollTop;
+           } else if( document.documentElement
+           && ( document.documentElement.scrollLeft
+            || document.documentElement.scrollTop ) ) {
+                   scrOfY = document.documentElement.scrollTop;
+           }
+           return scrOfY;
+}
+
+function marginMenuTop() {
+            var top  = getScrollTop();
+            var s    = document.getElementById("menu_left");
+            if (top+m2 < m1) {
+                s.style.top = (m1-top) + "px";
+            } else {
+                s.style.top = m2 + "px";
+            }
+}
+
+function setMenuPosition(){
+if(typeof window.addEventListener != "undefined"){
+    window.addEventListener("scroll", marginMenuTop, false);
+} else if(typeof window.attachEvent != "undefined"){
+    window. attachEvent("onscroll", marginMenuTop);
+}
+}
+
+if(typeof window.addEventListener != "undefined"){
+    window.addEventListener("load", setMenuPosition, false);
+} else if(typeof window.attachEvent != "undefined"){
+    window. attachEvent("onload", setMenuPosition);
+}
